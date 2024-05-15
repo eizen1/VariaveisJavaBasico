6a5816d21d4c9cc8391f751f4767bc1a94b0eecd
eizen1 <igorstraki@gmail.com>
2024-05-15 10:41:15 -0300

    tenario abreviado

--- VariaveisBasico/src/App.java
+++ VariaveisBasico/src/App.java
@@ -2,13 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
        int a, b;
        a = 5;
-       b = 5;
+       b = 6;
 
-       String resultado = "";
-       if (a ==b)
-            resultado ="verdadeiro";
-       else 
-            resultado = "falso";
+       String resultado = a==b ? "verdadeiro" : "falso";
 
             System.out.println(resultado);
     }
